6dcf46006255359774dfe676b4b59a50ca0a559a
Saibamen <saibamenppl@gmail.com>
2015-08-15 14:58:22 +0200

    Add more ignored strings

--- config.php
+++ config.php
@@ -12,11 +12,11 @@ $originLang = 'US|En';
 $fileExt = 'rc';
 
 // Ignore string list
-$ignoreString = array('OK', 'Ok', '&OK', '[OK]\n', 'ReactOS', 'MS Shell Dlg', 'Arial', 'Static', 'STATIC', 'popup', 'http://www.reactos.org');
+$ignoreString = array('OK', 'Ok', '&OK', '[OK]\n', 'ReactOS', 'MS Shell Dlg', 'Arial', 'Static', 'STATIC', 'popup', 'http://www.reactos.org', 'DUMMY', 'Tab1', 'List1', 'List2', 'List3', 'Slider1', 'Text1', 'Text2', 'Text3');
 
 if (!file_exists($ROSDir))
 {
     echo "ReactOS source path <b>$ROSDir</b> does not exist!";
-	exit;
+    exit;
 }
 ?>
